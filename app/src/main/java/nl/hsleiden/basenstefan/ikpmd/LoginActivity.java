1b8c53cbf7105a330836225f6dbbe0d90e357e80
Stefan <stefandekeijzer@xs4all.nl>
2019-02-18 12:08:27 +0100

    Cleanup

--- app/src/main/java/nl/hsleiden/basenstefan/ikpmd/LoginActivity.java
+++ app/src/main/java/nl/hsleiden/basenstefan/ikpmd/LoginActivity.java
@@ -1,36 +1,22 @@
 package nl.hsleiden.basenstefan.ikpmd;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.FragmentActivity;
+import android.support.design.widget.Snackbar;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.tasks.Task;
 
-/**
- * A login screen that offers login via email/password.
- */
-public class LoginActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener {
-
-    // UI references.
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
-    private View mProgressView;
-    private View mLoginFormView;
+public class LoginActivity extends Activity {
 
     GoogleSignInClient signInClient;
     private static final int RC_SIGN_IN = 9001;
@@ -53,9 +39,6 @@ public class LoginActivity extends FragmentActivity implements GoogleApiClient.O
                 login();
             }
         });
-
-        mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
     }
 
     private void login() {
@@ -66,11 +49,7 @@ public class LoginActivity extends FragmentActivity implements GoogleApiClient.O
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
         }
@@ -91,12 +70,9 @@ public class LoginActivity extends FragmentActivity implements GoogleApiClient.O
     }
 
     private void updateUI(GoogleSignInAccount signInAccount) {
-        Log.d("LOGIN", signInAccount.getDisplayName());
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        if (signInAccount != null) {
+            Snackbar.make(findViewById(R.id.sign_in_button), "Welkom: " + signInAccount.getDisplayName(), Snackbar.LENGTH_LONG).show();
+        }
     }
 }
 
