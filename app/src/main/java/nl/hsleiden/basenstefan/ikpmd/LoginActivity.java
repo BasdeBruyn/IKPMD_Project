de1ef93f45efbe2849c8f34d384db6df20a5593e
Stefan <stefandekeijzer@xs4all.nl>
2019-02-18 13:43:17 +0100

    Firebase users working finally

--- app/src/main/java/nl/hsleiden/basenstefan/ikpmd/LoginActivity.java
+++ app/src/main/java/nl/hsleiden/basenstefan/ikpmd/LoginActivity.java
@@ -3,7 +3,9 @@ package nl.hsleiden.basenstefan.ikpmd;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,62 +16,101 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends AppCompatActivity {
 
-    GoogleSignInClient signInClient;
+    private static final String TAG = "IKPMD";
+    GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth mAuth;
     private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("820611046383-hjb6rk3sic9fl4bubcvgevvvrd97ht40")
                 .requestEmail()
                 .build();
-        signInClient = GoogleSignIn.getClient(this, gso);
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        updateUI(account);
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
 
         SignInButton login = findViewById(R.id.sign_in_button);
         login.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                login();
+                signIn();
             }
         });
     }
 
-    private void login() {
-        Intent signInIntent = signInClient.getSignInIntent();
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                Snackbar.make(findViewById(R.id.sign_in_button), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                // ...
+            }
         }
     }
 
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
 
-            // Signed in successfully, show authenticated UI.
-            updateUI(account);
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w("IKPMD", "signInResult:failed code=" + e.getStatusCode());
-            updateUI(null);
-        }
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Snackbar.make(findViewById(R.id.sign_in_button), "Proberen in te loggen", Snackbar.LENGTH_SHORT).show();
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Snackbar.make(findViewById(R.id.sign_in_button), "Welkom: " + user.getDisplayName() , Snackbar.LENGTH_SHORT).show();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Snackbar.make(findViewById(R.id.sign_in_button), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
     }
 
-    private void updateUI(GoogleSignInAccount signInAccount) {
+    private void updateUI(FirebaseUser signInAccount) {
+        System.out.println(signInAccount);
         if (signInAccount != null) {
             Snackbar.make(findViewById(R.id.sign_in_button), "Welkom: " + signInAccount.getDisplayName(), Snackbar.LENGTH_LONG).show();
         }
