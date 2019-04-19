fe08a05c952192d73356d9cf7d2a6123a66b8b49
Stefan de Keijzer <stefandekeijzer@xs4all.nl>
2019-04-19 21:48:26 +0200

    Cleanup and offline work

---
+++ app/src/main/java/nl/hsleiden/basenstefan/ikpmd/DatabaseInfo.java
@@ -0,0 +1,18 @@
+package nl.mjboere.ikpmd_college2.Database;
+
+/**
+ * Created by mjboere on 6-12-2016.
+ */
+
+public class DatabaseInfo {
+
+    public class CarTables {
+        public static final String CARTABLE = "CarTable";   // NAAM VAN JE TABEL
+    }
+
+    public class CarColumn {
+        public static final String MERK  = "merk";	// VASTE WAARDES
+        public static final String PRIJS = "prijs";	// NAAM VAN DE KOLOMMEN
+        public static final String KLEUR = "kleur";	// FINAL !
+    }
+}
